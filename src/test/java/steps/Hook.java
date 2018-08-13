aebc9514721cf14c50694016d46048689914319f
pradipkumarpatil <42179318+pradipkumarpatil@users.noreply.github.com>
2018-08-14 01:25:30 +0530

    Update Hook.java

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -29,7 +29,7 @@ public class Hook extends BaseUtil{
 
 
         //Chrome driver
-        System.setProperty("webdriver.chrome.driver", "C:\Windows\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Windows\\chromedriver.exe");
         base.Driver = new ChromeDriver();
     }
 
